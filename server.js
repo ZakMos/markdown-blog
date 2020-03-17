59bdfcde0fa56657c111a6255d55d59d5f2584cd
ZakMos <z.zza@hotmail.com>
2020-03-17 10:07:00 +0100

    added slug

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const articleRouter = require('./routes/articles')
 const app = express()
 
 mongoose.connect('mongodb://localhost:27017/blog', {
-    useNewUrlParser: true, useUnifiedTopology: true
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 });
 
 app.set('view engine', 'ejs')
