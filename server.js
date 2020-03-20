a42b924c8b41d477d0369936883fa49cd471aa97
ZakMos <z.zza@hotmail.com>
2020-03-20 15:28:56 +0100

    added env

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config({path: __dirname + '/.env'})
+  }
 const express = require('express')
-const mongoose = require('mongoose')
-const Article = require('./models/article')
-const articleRouter = require('./routes/articles')
-const methodOverride = require('method-override')
 const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const Article = require('./models/article')
+const articleRouter = require('./routes/article')
+const methodOverride = require('method-override')
 
-mongoose.connect('mongodb://localhost:27017/blog', {
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
-});
+})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
 
 app.set('view engine', 'ejs')
-
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
 
@@ -18,9 +30,10 @@ app.get('/', async (req, res)=> {
     const articles = await Article.find().sort({
         createdAt: 'desc'
     })
-    res.render('articles/index', { articles: articles })
+    res.render('index', { articles: articles })
 })
 
-app.use('/articles', articleRouter)
+app.use('/', articleRouter)
 
-app.listen(5000)
+
+app.listen(process.env.PORT || 5000)
